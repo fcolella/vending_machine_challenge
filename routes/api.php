5cc768a386e149a0d75d7986e02960958b654a1d
fcolella <facundocolella@outlook.com>
2025-10-27 16:59:12 +0100

    Added API routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,13 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+/*Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum');*/
+
+use App\Infrastructure\Http\Controllers\VendingMachineController;
+
+Route::post('/insert-coin', [VendingMachineController::class, 'insertCoin']);
+Route::post('/return-coin', [VendingMachineController::class, 'returnCoin']);
+Route::post('/vend-item', [VendingMachineController::class, 'vendItem']);
+Route::post('/service/restock', [VendingMachineController::class, 'serviceRestock']);
